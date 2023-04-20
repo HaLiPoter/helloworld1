eda56d4e2e3cb5485b5e758d3c2bb7bb1b423af1
itcast <1441414846@qq.com>
2023-04-20 10:59:07 +0800

    init

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String Hello(){
+        System.out.println("hello world...");
+        return "hello.html";
+    }
+}
